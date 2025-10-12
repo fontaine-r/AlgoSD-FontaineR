d2776a0012e441f6d6d4c7ee90f6d9f838d4b472
fontaine-r <fontaine.romainp@gmail.com>
2025-10-12 10:39:04 +0200

    Update main.c

--- TD1/exo2/main.c
+++ TD1/exo2/main.c
@@ -1,1 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include "functions.h"
 
+
+void main(){
+    long long int n = 1000000 ;
+    int nstart = clock();
+    int a = NprimesUnderN(n);
+    int nend = clock();
+    int estart = clock();
+    int b = EprimesUnderN(n);
+    int eend = clock();
+    printf("Ils y a %i nombres premiers plus petits que %lli (naïf)\n", a, n);
+    printf("Temps naïf : %li s \n" , (nend-nstart)/CLOCKS_PER_SEC);
+    printf("Ils y a %i nombres premiers plus petits que %lli (efficace)\n", b, n);
+    printf("Temps naïf : %li s \n" , (eend-estart)/CLOCKS_PER_SEC);
+}
