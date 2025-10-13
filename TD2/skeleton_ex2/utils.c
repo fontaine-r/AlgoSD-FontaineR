62bf0182da0ab87d142afb6ef23e0d7033d9bfdb
fontaine-r <fontaine.romainp@gmail.com>
2025-10-13 09:42:30 +0200

    TD2

--- TD2/skeleton_ex2/utils.c
+++ TD2/skeleton_ex2/utils.c
@@ -1,17 +1,18 @@
 #include "utils.h"
 
-void swap_int(int *a, int *b)
-{
-    // swap two elements
+void swap_int(int *a, int *b){
+    int c = &a;
+    a = &b;
+    b = c;
 }
 
-bool is_sorted_nondecreasing(int *arr, int n)
-{
-    // check if array is sorted, if yes return true
-    // if not return false
+bool is_sorted_nondecreasing(int *arr, int n){
+    for (int i = 0; i<n-1; i++){
+        if (arr[i]>arr[i+1]) return false;
+    }
+    return true;
 }
 
-void copy_array(int *src, int *dst, int n)
-{
+void copy_array(int *src, int *dst, int n){
     // copy array elements src into dst
 }
