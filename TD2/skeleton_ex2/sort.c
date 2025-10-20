685e27d7530042e7e3946aac064bd64ed245baf0
fontaine-r <fontaine.romainp@gmail.com>
2025-10-20 15:00:30 +0200

    TD2

--- TD2/skeleton_ex2/sort.c
+++ TD2/skeleton_ex2/sort.c
@@ -4,12 +4,26 @@
 
 void selection_sort(int *arr, int n)
 {
-    // selection sort iterative
+    for (int i = 0; i<n-1;i++){
+        int min = i;
+        for (int j=i+1 ; j<n ; j++){
+            if (arr[j]== arr[min]) min = j;
+        }
+        swap_int(arr[i],arr[min]);
+    }
 }
 
 void insertion_sort(int *arr, int n)
 {
-    // insertion sort iterative
+    for (int i = 0 ; i<n ; i++){
+        int key = arr[i];
+        int j = i-1;
+        while (j>0){
+            arr[j+1] = arr[j];
+            j-=1;
+        }
+        arr[j+1] = key;
+    }
 }
 
 void bubble_sort(int *arr, int n)
